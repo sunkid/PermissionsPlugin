834a5427c35fefd4485a13b91504bd6731c9b841
sunkid <sunkid@iminurnetz.com>
2011-05-03 12:57:08 -0700

    directory cleanup

---
+++ src/test/java/com/iminurnetz/bukkit/plugin/permissions/model/tests/TestPlayerGroupModel.java
@@ -0,0 +1,11 @@
+package com.iminurnetz.bukkit.plugin.permissions.model.tests;
+
+import junit.framework.TestCase;
+
+public class TestPlayerGroupModel extends TestCase {
+
+    public void testNothing() {
+        assertTrue(true);
+    }
+
+}
